4356a3b0bb4ad4ceb9ca549bf8de4b888b042cdc
Bahareh Amricachi <baharehamri@gmail.com>
2023-02-09 12:24:52 -0800

    Initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declaring initalize variables
+string name = "Wiggum";
+int age = 75;
+char middleInitial = 'K';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
